56b01b0cf5b43605eb806adee01fa5087d782ad7
Sharmaine <sharmaine.borbe@students.dominican.edu>
2024-03-12 13:58:22 -0700

    ugh

--- pages/tasks.jsx
+++ pages/tasks.jsx
@@ -1,11 +1,8 @@
-import { TodoWrapper } from '@/components/ToDo/TodoWrapper'
 import React from 'react'
 
 const tasks = () => {
   return (
-    <div>
-      <TodoWrapper />
-    </div>
+    <div>tasks</div>
   )
 }
 
