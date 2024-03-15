fddd99b5aea9b22543e26f15cfe08dd0248bfb97
Sharmaine <sharmaine.borbe@students.dominican.edu>
2024-03-14 20:35:38 -0700

    style updates

--- components/Dashboard.jsx
+++ components/Dashboard.jsx
@@ -7,13 +7,13 @@ import QuickLinksCard from './QuickLinksCard';
 
 const Dashboard = () => {
   return (
-    <div className="grid grid-cols-3 gap-4 bg-zinc-900">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 bg-zinc-900">
       <PokemonCard />
       <WeatherCard apiKey={process.env.NEXT_PUBLIC_OPENWEATHERMAP_API_KEY} />
       <TodoWrapper />
-      <div className="col-span-3 flex">
-        <NasaCard />
-        <QuickLinksCard />
+      <div className="col-span-1 md:col-span-2 lg:col-span-3 flex flex-col md:flex-row">
+        <NasaCard className="md:w-1/2" />
+        <QuickLinksCard className="md:w-1/2" />
       </div>
     </div>
   );
